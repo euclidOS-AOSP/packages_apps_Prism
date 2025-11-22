ea4c22a8f20d7fa0d0c2263daaade364ade0cf9a
Mrick343 <mrinmoy8429de@gmail.com>
2025-11-23 02:29:49 +0530

    Prism: Init empty Settings
    Co-authored-by: DylanAkp <dylan.akpinar@epitech.eu>
    Co-authored-by: ShukakuZa <yusufzuzu787@gmail.com>
    Co-authored-by: Mrick343 <mrinmoy8429de@gmail.com>
    Co-authored-by: Zabuka_zuzu <zuzu950444@gmail.com>

---
+++ src/com/prism/settings/PrismSettings.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2025  euclidOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.prism.settings;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.search.SearchIndexable;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.dashboard.DashboardFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
+public class PrismSettings extends DashboardFragment {
+
+    private static final String LOG_TAG = "Prism";
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.prism_settings;
+    }
+
+      @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.PRISM;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected String getLogTag() {
+        return LOG_TAG;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, this /* fragment */, getSettingsLifecycle());
+    }
+    
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, PrismSettings fragment, Lifecycle lifecycle) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new PrismSettingsController(context));
+        return controllers;
+    }
+
+    /**
+     * For Search.
+     */
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.prism_settings) {
+                @Override
+                public List<AbstractPreferenceController> createPreferenceControllers(
+                        Context context) {
+                    return buildPreferenceControllers(context, null /* fragment */,
+                            null /* lifecycle */);
+                }
+            };
+}
