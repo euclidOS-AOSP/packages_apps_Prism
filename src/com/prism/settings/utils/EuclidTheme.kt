c33612ff1778ad0a996f95e260882e61631339ac
Ghosuto <clash.raja10@gmail.com>
2025-12-21 15:38:38 +0000

    Switch to Axion color picker implementation
    - rmp22 for base implementation https://github.com/AxionAOSP/android_packages_apps_AxThemePicker
    - Ghost adapt for settings from AxThemePicker
    - Skydoves for olor picker library https://github.com/skydoves/colorpicker-compose
    
    Change-Id: I460b6b78b83798924acee5c4964c971f0cdf8ac1
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Co-authored-by: Jaewoong Eum <skydoves@naver.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

---
+++ src/com/prism/settings/utils/EuclidTheme.kt
@@ -0,0 +1,26 @@
+package com.prism.settings.utils
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun EuclidTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val context = LocalContext.current
+    
+    val colorScheme = if (darkTheme) {
+        dynamicDarkColorScheme(context)
+    } else {
+        dynamicLightColorScheme(context)
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography(),
+        content = content
+    )
+}
