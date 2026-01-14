cc2da58b53d1bc2c774d08f50241e37fc4d0e5f1
Pranav Vashi <neobuddy89@gmail.com>
2026-01-14 19:03:37 +0000

    Prism: add omnijaws lockscreen customization
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- src/com/prism/settings/fragments/lockscreen/Lockscreen.java
+++ src/com/prism/settings/fragments/lockscreen/Lockscreen.java
@@ -1,64 +1,62 @@
 package com.prism.settings.fragments.lockscreen;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.os.RemoteException;
-import android.os.ServiceManager;
-import android.os.UserHandle;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.hardware.fingerprint.FingerprintManager;
-import android.content.res.Resources;
-import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceGroup;
-import android.content.SharedPreferences;
-import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
-import android.provider.Settings;
-import com.android.settings.R;
-
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.IWindowManager;
-import android.view.View;
-import android.view.WindowManagerGlobal;
-
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.Utils;
-import android.util.Log;
+import com.android.settings.R;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
+// OmniJaws
+import com.android.internal.util.euclid.OmniJawsClient;
 
 public class Lockscreen extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+    
+    private static final String KEY_WEATHER = "lockscreen_weather_enabled";
+
+    private Preference mWeather;
+    private OmniJawsClient mWeatherClient;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        addPreferencesFromResource(R.xml.lockscreen_settings);
+        Context context = getContext();
+
+        // Initialize Weather Settings
+        mWeather = findPreference(KEY_WEATHER);
+        mWeatherClient = new OmniJawsClient(context);
+        updateWeatherSettings();
+
+        requireActivity().setTitle(R.string.prism_lockscreen_dashboard_title);
+    }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         return false;
     }
 
-    @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.PRISM;
+    // Handles updating the weather summary dynamically
+    private void updateWeatherSettings() {
+        if (mWeatherClient == null || mWeather == null) return;
+
+        boolean weatherEnabled = mWeatherClient.isOmniJawsEnabled();
+        mWeather.setEnabled(weatherEnabled);
+        mWeather.setSummary(weatherEnabled
+                ? R.string.lockscreen_weather_summary
+                : R.string.lockscreen_weather_enabled_info);
     }
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.lockscreen_settings, rootKey);
+    public void onResume() {
+        super.onResume();
+        updateWeatherSettings();
+    }
 
-        getActivity().setTitle(R.string.prism_lockscreen_dashboard_title);
-
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.PRISM; 
     }
 }
